f7c3f5fc81162a7ca59529c221dd79b83516a9dd
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-05-01 01:26:33 -0700

    Check if game is started

--- app/javascript/controllers/type_controller.js
+++ app/javascript/controllers/type_controller.js
@@ -3,7 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="type"
 export default class extends Controller {
   connect() {
+    this.gameStarted = false
   }
 
+  inputChanged(e) {
+    console.log(e.target.value)
+    console.log(this.gameStarted)
+    if (e.target.value != null && this.gameStarted === false) {
+      this.gameStarted = true
+    }
+  }
 
 }
